2aec34985d883d7a43cc78466b85833b8c07d814
Seth Miller <millerado@gmail.com>
2022-08-12 13:46:26 -0600

    removed unneed code

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // Constants
 const API_KEY = '8dae7ca41db9eb1c69c7ee17d1924999';
 const WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
-const GEOCODING_URL = 'http://api.openweathermap.org/geo/1.0/direct?';
+
 
 // States
-let inputCitylat = 0;
-let inputCitylon = 0;
 
 // Cached Element References
 const $main = $('main');
@@ -33,21 +31,6 @@ function handleSubmit(event) {
       console.log('Weather Error: ', error);
     }
   );
-}
-
-// function getCityCoord(event) {
-//   const promise = $.ajax(`${GEOCODING_URL}q=${city}&appid=${API_KEY}`);
-//   promise.then(
-//     (data) => {
-//       console.log('City Data: ', data);
-//       inputCitylat = data[0].lat;
-//       inputCitylon = data[0].lon;
-//     },
-//     (error) => {
-//       console.log('City Error: ', error);
-//     }
-//   );
-// }
 
 function render(weatherData) {
   $main.html(`
